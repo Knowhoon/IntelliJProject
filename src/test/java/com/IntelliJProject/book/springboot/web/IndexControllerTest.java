95174c5c51946ec0103a4a9f5c371d4ed8993451
knowhoon <knowhoon@gmail.com>
2021-11-05 15:21:52 +0900

    게시판 crud 기능 추가

--- src/test/java/com/IntelliJProject/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/IntelliJProject/book/springboot/web/IndexControllerTest.java
@@ -1,2 +1,27 @@
-package com.IntelliJProject.book.springboot.web;public class IndexControllerTest {
+package com.IntelliJProject.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mainPage_Loding(){
+        //when
+        String body = this.restTemplate.getForObject("/",String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트 웹서비스 시작");
+
+    }
 }
